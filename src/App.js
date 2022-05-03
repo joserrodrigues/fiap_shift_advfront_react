51ca5b8de5bd08d513e59cdcbe7c8cce8974ce9e
rubens Rodrigues <rubens@schoolguardian.app>
2022-05-03 00:51:36 -0300

    Aula 1 - Parte 1 - Regras do JSX

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const testeJSX = <div className="App"> Outro Codigo </div>
+
+  return (
+    <div className="container">
+      <div className="App">Hello World
+      </div>
+      {testeJSX}
+    </div>
+  );
+}
+
+export default App;
