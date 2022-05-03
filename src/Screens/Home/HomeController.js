b41c83d8fde6881abd1924fee323f6e219bb667f
rubens Rodrigues <rubens@schoolguardian.app>
2022-05-03 01:06:54 -0300

    Aula 2 - Parte 1 - States

---
+++ src/Screens/Home/HomeController.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import HomeView from './HomeView'
+
+class HomeController extends React.Component {
+
+    constructor() {
+        super();        
+        this.state = {
+            count: 0
+        };
+
+        setInterval(() => {
+            this.setState({
+                count: this.state.count + 1
+            })
+        }, 1000);
+    }
+
+
+
+    render() {
+        return (
+            <HomeView count={this.state.count}/> //Chamando o View
+        )
+    }
+}
+export default HomeController;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import HomeController from './Screens/Home/HomeController';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomeController />
   </React.StrictMode>
 );
 
