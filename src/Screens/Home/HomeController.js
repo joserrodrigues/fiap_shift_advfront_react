412d18f8a0195a02ff05cafbdfffb71a398de0a8
rubens Rodrigues <rubens@schoolguardian.app>
2022-05-04 18:50:32 -0300

    Aula 5 - Parte 1 - useContext

--- src/Screens/Home/HomeController.js
+++ src/Screens/Home/HomeController.js
@@ -1,27 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import useAPI from '../../Services/APIs/Common/useAPI';
 import toys from '../../Services/APIs/Toys/toys';
 import HomeView from './HomeView';
 import { useNavigate } from "react-router-dom";
+import { InfoContext } from "../../store/InfoContext";
 
-
-export default function HomeController() {
+const HomeController = () => {
 
     const getToysGetAPI = useAPI(toys.getAllToys);
     const navigate = useNavigate();
+    const userCoordinates = useRef(null);
+    const context = useContext(InfoContext);
+
+    console.log(context);
+
 
     useEffect(() => {
         getToysGetAPI.request(1);
     }, []);
 
+    console.log(userCoordinates);
     const goToPage = (toy) => {
-        navigate("/detail/"+toy._id,{
-            state:{
-                toy: JSON.stringify(toy)
-            }            
+
+        navigate("/detail/" + toy._id, {
+            state: {
+                toy: JSON.stringify(toy),
+                latitude: userCoordinates.current ? userCoordinates.current.latitude : 0,
+                longitude: userCoordinates.current ? userCoordinates.current.longitude : 0
+            }
         })
     }
 
     console.log(getToysGetAPI.data)
-    return <HomeView arrayToys={getToysGetAPI.data} loading={getToysGetAPI.loading} goToPage={goToPage} />
+    return <HomeView arrayToys={getToysGetAPI.data} loading={getToysGetAPI.loading} goToPage={goToPage} info={context.info} />
 }
+export default HomeController;
--- src/Screens/Home/HomeView.js
+++ src/Screens/Home/HomeView.js
@@ -1,46 +1,66 @@
 import * as React from 'react';
 import './Home.css'
-import { Box, Container, CircularProgress, Grid, Typography } from '@mui/material';
+import { Box, Container, CircularProgress, Grid, Typography, Button, Stack } from '@mui/material';
+import { Card, CardContent, CardMedia } from '@mui/material';
+import CardInfo from '../../Components/CardInfo/CardInfo'
+import Header from '../../Components/Header/Header';
 
-export default function HomeView({ loading, arrayToys, goToPage }) {
+export default function HomeView({ loading, arrayToys, goToPage, info }) {
 
-    let name = "";
     let infoBox = [];
 
-    if (loading){
+    if (loading) {
         infoBox.push(
             <Grid key={1} item lg={12} xl={12} className="itemClass">
                 <CircularProgress />
-            </Grid>            
-            
+            </Grid>
+
         )
-    } else  if (arrayToys) {
+    } else if (arrayToys) {
         arrayToys.toys.forEach(toy => {
             infoBox.push(
-                <Grid key={toy._id} item lg={12} xl={12} className="itemClass">
-                    <Typography gutterBottom variant="body" className="text" onClick={() => goToPage(toy)}>
-                        Info {toy.name}
-                    </Typography>
+                <Grid key={toy._id} item sm={12} md={6} lg={2} xl={2} className="itemClass" onClick={() => goToPage(toy)}>
+                    <Card className='cardClass'>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            src={toy.mainImage}
+                            alt={toy.name}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="body2" component="div" className='titleCard'>
+                                {toy.name}
+                            </Typography>
+                            <CardInfo />
+                        </CardContent>
+                    </Card>
                 </Grid>
             )
         });
     }
     return (
         <Container fixed className="container" maxWidth="lg">
-            <Box className="contentBox">
-            <Grid
-                container
-                spacing={0}
-                direction="row">
-                    <Grid item lg={12} xl={12} >
-                        <Typography gutterBottom variant="h1" className="text">
-                            Base de Brinquedos {name}
-                        </Typography>
-                    </Grid>
-                    {infoBox}
+            <Stack>
+                <Header />
+                <Box className="contentBox">
+                    <Grid
+                        container
+                        spacing={3}
+                        alignItems="center"
+                    >
+                        <Grid item lg={6} xl={6} className="titlePage">
+                            <Typography variant="h1" >
+                                Base de Brinquedos - {info}
+                            </Typography>
+                        </Grid>
+                        <Grid item lg={6} xl={6} className="titleButton">
+                            <Button variant='primary' className='buttonClass'>Cadastrar brinquedo</Button>
+                        </Grid>
+                        {infoBox}
 
-            </Grid>
-            </Box>            
+                    </Grid>
+                </Box>
+            </Stack>
         </Container>
     );
 }
