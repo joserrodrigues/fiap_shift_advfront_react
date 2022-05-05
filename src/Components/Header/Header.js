dab393d9361c18ef4ead6b1f4ccf57ec6638c3ce
rubens Rodrigues <rubens@schoolguardian.app>
2022-05-05 11:50:26 -0300

    Aula 7 - Parte 2 - Notificação

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,8 @@ import { InfoContext } from '../../store/InfoContext';
 import { Typography, Grid, Stack } from '@mui/material';
 import logoImg from '../../Images/Logo.png'
 import './Header.css'
+import { ToastContainer } from 'react-toastify';
+
 
 const Header = () => {
 
@@ -11,16 +13,17 @@ const Header = () => {
 
     return (
         <>
+            <ToastContainer />
             <Grid
                 container
                 spacing={3}
                 alignItems="center"
                 className='gridHeader'
             >
-                <Grid item xs={4} md={6} xl={6} className="leftHeader">
-                    <img src={logoImg} alt="children" width={'50px'} className="logo Img" />
+                <Grid item xs={12} md={6} xl={6} className="leftHeader">
+                    <img src={logoImg} alt="children" width={'50px'} className="logo Img" />     
                 </Grid>
-                <Grid item xs={8} md={6} xl={6} className="rightHeader">
+                <Grid item xs={12} md={6} xl={6} className="rightHeader">
                     <Stack direction="row" className="rightHeader">
                         <Stack direction="column" className="rightHeader">
                             <Typography variant="h2" >
@@ -31,16 +34,16 @@ const Header = () => {
                                     Logout
                                 </Typography>
                             </div>
-
+                            
                         </Stack>
-
+                        
                         <div className='headerUserLogoInfo' onClick={() => context.onChangeInfo('Mudou')}>
                             <Typography variant="h1" className='iconFont' >{userInitial}</Typography>
                         </div>
                     </Stack>
-
+                    
                 </Grid>
-            </Grid>
+            </Grid>            
         </>
     );
 };
--- src/Routes/RouteController.js
+++ src/Routes/RouteController.js
@@ -1,14 +1,18 @@
 import React, { useContext } from 'react';
 import HomeController from '../Screens/Home/HomeController';
-import DetailController from '../Screens/Detail/DetailController';
 import LoginController from '../Screens/Login/LoginController';
 import { Routes, Route } from "react-router-dom";
 import { InfoContext } from '../store/InfoContext';
 import AddController from '../Screens/Add/AddController';
+import DetailController from '../Screens/Detail/DetailController'
+import { getNotificationToken, onMessageListener } from '../Services/Firebase/ManageFirebase';
 
 const RouteController = () => {
-    
+
     const { tokenLogin, onMakeLogin } = useContext(InfoContext);
+
+    getNotificationToken();
+    onMessageListener();
 
     let hasToken = false;
     if (tokenLogin === undefined || tokenLogin === null) {
@@ -38,7 +42,7 @@ const RouteController = () => {
                 <Route path="*" element={<LoginController />} />
             </Routes>
         );
-    } 
+    }
 };
 
 export default RouteController;
